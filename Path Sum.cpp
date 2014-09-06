05f8741f4eef8c2dc3111fc68d147d80e73b1efb
mingitouch <mingitouch@gmail.com>
2014-09-06 21:03:27 +0800

    递归
    递归，需要好好看看

---
+++ Path Sum.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(!root)
+            return false;
+        if(!root->left and !root->right and root->val == sum)
+            return true;
+        return hasPathSum(root->left, sum-root->val) or hasPathSum(root->right, sum-root->val);
+    }
+};
